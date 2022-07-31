7984617f73e28e5d13134ec3b3449aac702f1ad1
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 08:07:03 +0600

    Variable naming convention

--- variable.js
+++ variable.js
@@ -7,7 +7,7 @@ var number = 123;
 ## 5 things needed to declare a variable in JS
 1. Keyword (var, let, const)
 2. Variable Name
-3. Assignment oparator Equal sign (=)
+3. Assignment oparator / Equal sign (=)
 4. Variable value
 5. Semicolon (;)
 
@@ -27,3 +27,38 @@ var designation = 'Frontend Developer';
 // Boolean
 var single = true;
 var married = false;
+
+
+// ### Variable name naming convention
+
+// 1. Variable name can not be any keyword
+var var = name;
+var false = lier;
+
+// 2. variable name can not have quotation
+var "name" = "Shams Sujon";
+
+// 3. variable name can not start with a number
+var 101rules = "LOL";
+var 1mark = "kala";
+
+// 4. variable name has to be in one word. NO space
+var my full name = "Shamsul Islam Sujon";
+
+// 5. Can not use dash
+var user-name = "Yusra";
+
+// 6. No uppercase keyword
+VAR name1 = "Lalu Kosai";
+Var name2 = "Kalu Kosai";
+vaR name3 = "Malu Kosai";
+
+// 7. Varible name is case sensitive
+var number = 100;
+var Number = 100;
+
+// 8. How to use long names
+var mycurrentlocation = "Kushtia";
+var my_current_location = "Kushtia"; // Snake case
+var myCurrentLocation = "Kushtia"; // Camel case, most used.
+var MyCurrentLocation = "Kushtia"; // Pascal case
