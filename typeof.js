809e81200272840770d70eb56e151bd45f7c6e2c
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 09:00:35 +0600

    type of

---
+++ typeof.js
@@ -0,0 +1,27 @@
+var myName = "Sujon";
+console.log(typeof myName);
+
+var age = 27;
+console.log(typeof age);
+
+var married = false;
+console.log(typeof married);
+
+var employment;
+console.log(employment);
+
+
+
+//::: Advanced :::\\
+// Problem
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+console.log(sum);
+
+// Solution
+sum = sum.toFixed(1);
+console.log(sum);
+
+sum = parseFloat(sum);
+console.log(sum);
