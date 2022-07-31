2c3939b694549eba63482e3d67b6f744ce0ca9a0
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 08:37:54 +0600

    String concatanation

---
+++ concatanation.js
@@ -0,0 +1,9 @@
+var firstName = "Shams";
+var lastName = "Sujon";
+var fullName = firstName + lastName;
+console.log(fullName);
+
+var abburFirstName = "Kamal";
+var abburLastName = "Kha";
+var abburFullName = abburFirstName + " " + abburLastName;
+console.log(abburFullName);
