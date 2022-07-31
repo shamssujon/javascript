da3e07b1b621d62254fe86c5a42aa9b5a75c4ed5
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 09:16:17 +0600

    practice 2 solved

---
+++ practice-2.js
@@ -0,0 +1,11 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks = math + biology + chemistry + physics + bangla;
+var averageMark = totalMarks / 5;
+
+// console.log(totalMarks);
+console.log(averageMark.toFixed(2));
