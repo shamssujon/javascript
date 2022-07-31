16cc5fbb93cb00ee18cf9e8857c91bba2ee43c1f
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 08:47:13 +0600

    String to number conversion

---
+++ conversion.js
@@ -0,0 +1,21 @@
+var number1 = 100;
+var number2 = "200";
+
+var totalNumber = number1 + number2;
+console.log(totalNumber);
+
+// ::::::::::::::::::::::::::::::: \\
+
+var number1 = "100";
+var number2 = 200;
+
+var totalNumber = number1 + number2;
+console.log(totalNumber);
+
+// Convert string to number
+console.log(parseInt(totalNumber));
+
+
+var gpa = "4.88";
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
