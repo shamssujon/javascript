546059744164ef952f75bb595d9936cd707463fe
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 08:17:59 +0600

    Math basic operations

---
+++ operation.js
@@ -0,0 +1,19 @@
+var price1 = 100;
+var price2 = 30;
+
+console.log(price1);
+console.log(price2);
+console.log(price1 + price2);
+console.log(price1 - price2);
+
+var team = 10;
+var member = 11;
+
+var totalPlayer = team * member;
+console.log(totalPlayer);
+
+var amount = 500;
+var person = 3;
+
+var perPersonAmount = amount / person;
+console.log(perPersonAmount);
