4740a17437307f958af911d790726e5c1800660e
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 07:32:06 +0600

    Basic math

---
+++ basic-math.js
@@ -0,0 +1,20 @@
+/*
+
+* 1. Number ==> [10, 5000, -67, 81.77]
+* 2. Odd (বিজোড়), Even (জোড়)
+* 3. Integer (পূর্ণসংখ্যা) ==> [3, 56, -42]
+* 4. Float, Decimal (ভগ্নাংশ) ==> [33.4, 77.94, -33.333]
+* 5. Operation: Addition (যোগ), Substruction (বিয়োগ), Multiplication (গুন), Division (ভাগ), Remainder (ভাগশেষ)
+* 6. Percentage (শতকরা), Avarage (গড়)
+* 7. Comparision (তুলনা) ==> [> (বড়), < (ছোট), == (সমান), != (সমান না), >= (বড় বা সমান), <= (ছোট বা সমান)]
+* 8. Uniform rules (ঐকিক নিয়ম)
+* 9. Series number operation 
+
+
+### Extra ###
+* 1. Prime number
+* 2. Permutation
+
+
+### যদি কিছু লাগে শিখে নেব 
+*/
