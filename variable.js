9429fdf808fe1b5879d22d6ab577fc1c98bafa28
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 07:44:40 +0600

    Variable

---
+++ variable.js
@@ -0,0 +1,18 @@
+// vary + able = variable
+
+var number = 123;
+
+/* 
+
+## 5 things needed to declare a variable in JS
+1. Keyword (var, let, const)
+2. Variable Name
+3. Assignment oparator Equal sign (=)
+4. Variable value
+5. Semicolon (;)
+
+*/
+
+var age = 27;
+var height = 5.5;
+var weight = 70;
