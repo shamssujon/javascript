05fbc10b48e7ee3818783e890063b2aa7480d5b4
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 09:16:10 +0600

    practice 1 solved

---
+++ practice-1.js
@@ -0,0 +1,7 @@
+var horirMaDilo = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalPrice = applePrice + orangePrice;
+var takaBecheGelo = horirMaDilo - totalPrice;
+console.log(takaBecheGelo);
