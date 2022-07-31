afd1d55fecf523d220d06ef713c31b4466148b39
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 08:30:32 +0600

    variable math operation shorthand

---
+++ shorthand.js
@@ -0,0 +1,30 @@
+// Variable declaration
+var dollerRate = 78;
+
+// Variable value update, set a new value
+dollerRate = 105;
+
+// Add something to the existing value
+dollerRate = dollerRate + 5;
+
+console.log(dollerRate);
+
+// Addition to the same variable shorthand
+dollerRate += 2; // same as dollerRate = dollerRate + 2;
+console.log(dollerRate);
+
+// Substruction to the same variable shorthand
+dollerRate -= 4; // same as dollerRate = dollerRate - 4;
+console.log(dollerRate);
+
+// Multiplication to the same variable shorthand
+dollerRate *= 2; // same as dollerRate = dollerRate * 2;
+console.log(dollerRate);
+
+// Division to the same variable shorthand
+dollerRate /= 2; // same as dollerRate = dollerRate / 2;
+console.log(dollerRate);
+
+// Increase/Decrease value by 1
+dollerRate++; // same as dollerRate = dollerRate + 1;
+dollerRate--; // same as dollerRate = dollerRate - 1;
