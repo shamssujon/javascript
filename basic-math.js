d4e03f866202e0c145ec1c2a6f08175af887a301
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-07-31 07:34:25 +0600

    Update basic math

--- basic-math.js
+++ basic-math.js
@@ -1,7 +1,7 @@
 /*
 
 * 1. Number ==> [10, 5000, -67, 81.77]
-* 2. Odd (বিজোড়), Even (জোড়)
+* 2. Odd (বিজোড়), Even (জোড়), Positive, Negetive
 * 3. Integer (পূর্ণসংখ্যা) ==> [3, 56, -42]
 * 4. Float, Decimal (ভগ্নাংশ) ==> [33.4, 77.94, -33.333]
 * 5. Operation: Addition (যোগ), Substruction (বিয়োগ), Multiplication (গুন), Division (ভাগ), Remainder (ভাগশেষ)
@@ -14,6 +14,7 @@
 ### Extra ###
 * 1. Prime number
 * 2. Permutation
+* 3. Fibonacci sequence
 
 
 ### যদি কিছু লাগে শিখে নেব 
