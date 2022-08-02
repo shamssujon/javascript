403dbc3be791d2a5d0538e7d5f5e1214b979ce14
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-08-02 16:49:41 +0600

    Comparison

---
+++ comparison.js
@@ -0,0 +1,17 @@
+// Grater than >
+console.log(5 > 6);
+
+// Less than <
+console.log(5 < 6);
+
+// Equal == , ===
+console.log(5 == 6);
+
+// Not equal != , !==
+console.log(5 != 6);
+
+// Grater than or equal >=
+console.log(5 >= 10);
+
+// Less than or equal <=
+console.log(5 <= 5);
