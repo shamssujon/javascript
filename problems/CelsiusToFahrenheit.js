58fc0b25c59b522676fb5813756dbf76f2bd89b1
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-08-10 21:26:37 +0600

    Celsius to Fahrenheit, Fahrenheit to Celsius

---
+++ problems/CelsiusToFahrenheit.js
@@ -0,0 +1,5 @@
+var celcius = 100;
+
+var fahrenheit = (celcius * 1.8) + 32;
+
+console.log(celcius, "deg Celcius = ", fahrenheit, "deg Fahrenheit");
---
+++ problems/FahrenheitToCelsius.js
@@ -0,0 +1,7 @@
+var fahrenheit = 100;
+
+celsius = (fahrenheit - 32) / 1.8;
+
+celsiusRounded = parseFloat(celsius.toFixed(3)) ;
+
+console.log(fahrenheit, 'deg Fahrenheit is equvalent to', celsiusRounded, 'deg Celsius');
